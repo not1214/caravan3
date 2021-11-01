f6ebe9f7740a161a05696a58c23a6cdf59318b83
not1214 <91748221+not1214@users.noreply.github.com>
2021-11-01 12:18:35 +0900

    Finish caravan

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,9 +9,12 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blog_path(blog.id)
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(blog.id)
+    else 
+      render :new
+    end
   end
 
 
@@ -27,6 +30,12 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog.id)
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
   end
 
   private
